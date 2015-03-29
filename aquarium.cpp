72e432f96dda83fe25815d556d607b46cdc72b32
Mehmet Atasoy <mehmetatasoy_@hotmail.com>
2015-03-29 23:49:06 +0300

    Update aquarium.cpp
    Time zone set to GMT+3
    New times set for daylights and moon lights.
    Unnecessary codes deleted in loop function.
    Daylight dim set for %50 max and wpm method reversed because of pwm circuit for daylight.
    moonlight dim disabled.

--- aquarium.cpp
+++ aquarium.cpp
@@ -17,15 +17,12 @@ byte mac[] = { 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF };
 IPAddress timeServer(132, 163, 4, 101);
 
 /* Set this to the offset (in seconds) to your local time
-   This example is GMT - 6 */
-const long timeZoneOffset = 7200L;   
+   This example is GMT + 3 */
+const long timeZoneOffset = 10800L;   
 
-/* Syncs to NTP server every 15 seconds for testing, 
-   set to 1 hour or more to be reasonable */
-unsigned int ntpSyncTime = 15;         
+/* Syncs to NTP server every minute */
+unsigned int ntpSyncTime = 60;         
 
-
-/* ALTER THESE VARIABLES AT YOUR OWN RISK */
 // local port to listen for UDP packets
 unsigned int localPort = 8888;
 // NTP time stamp is in the first 48 bytes of the message
@@ -39,8 +36,6 @@ unsigned long ntpLastUpdate = 0;
 // Check last time clock displayed (Not in Production)
 time_t prevDisplay = 0;             
 
-unsigned long oneDay = 86400;
-
 /******************************************************************/
 
 // initialize the library with the numbers of the interface pins
@@ -49,10 +44,10 @@ LiquidCrystal lcd(8,   9,  4,  5,  6,  7);
 // Initialize Arduino Ethernet Client
 EthernetClient client;
 
+// ThingSpeak Setup
 char thingSpeakAddress[] = "api.thingspeak.com";
-const int updateThingSpeakInterval = 5 * 1000;      // Time interval in milliseconds to update ThingSpeak (number of seconds * 1000 = interval)
-
-// Variable Setup
+const int updateThingSpeakInterval = 60 * 1000;      // Time interval in milliseconds to update ThingSpeak (number of seconds * 1000 = interval)
+String myThingSpeakKey = "POJ06PXJKR76NB39";  
 long lastConnectionTime = 0; 
 boolean lastConnected = false;
 int failedCounter = 0;
@@ -63,8 +58,8 @@ int failedCounter = 0;
 #define CO2 26 // digital
 #define FANS  28 // digital
 #define HEATER 30 // digital
-#define LIGHTS_PWM 2// analog
-#define MOONLIGHTS_PWM 3 // analog
+#define LIGHTS_PWM 2// digital
+#define MOONLIGHTS_PWM 3 // digital
 #define PWM_MAX 255
 #define PWM_MIN 0
 
@@ -72,10 +67,10 @@ int failedCounter = 0;
 const unsigned long HOUR = 60 * 60;
 const unsigned long MINUTE = 60;
 const int TARGET_BRIGHTNESS = 255;
+const int DESIRED_BRIGHTNESS = (255 * 5) / 10;
 
 float temperature = 0;
 
-#define thingSpeakApiKey "XXXXXXXX"
 OneWire  ds(A8); // watertemp
 
 byte water[8] = //icon for water droplet
@@ -88,6 +83,18 @@ byte water[8] = //icon for water droplet
     B10001,
     B10001,
     B01110,
+};
+
+byte celsiusSymbol[8] = //icon for water droplet
+{
+    B01000,
+    B10100,
+    B01000,
+    B00011,
+    B00100,
+    B00100,
+    B00100,
+    B00011,
 };
 
 void welcomeLcd(){
@@ -126,9 +133,6 @@ void displaySystem(float temp, int co2Status){
   print2digits(second());
   addCelciusSymbol();	
   lcd.setCursor(10,2);
-
-// burda delay cekmek yerine, milis farkindan ekrani yenilemek daha dogru, delay ledlerde kisilmalara yol aciyor.
-  //delay(3000);
 	
 }
 
@@ -173,7 +177,7 @@ float checkWaterTemp(){
  
   ds.reset();
   ds.select(addr);
-  ds.write(0x44, 1);        // start conversion, with parasite power on at the end
+  ds.write(0x44, 1);  // start conversion, with parasite power on at the end
   
   delay(1500);     // maybe 750ms is enough, maybe not
   // we might do a ds.depower() here, but the reset will take care of it.
@@ -217,10 +221,10 @@ float checkWaterTemp(){
 //Heater Control 
 void heaterControl(float temp){
   
-  if(temp > 0 && temp < 26){ //>0 to avoid -999.00*C error   
+  if(temp > 0 && temp < 25){ //>0 to avoid -999.00*C error   
     digitalWrite(HEATER, HIGH);
     //Serial.println("Heating..."); 
-  } else if(temp > 26 && temp < 50){ //<0 to avoid -999.00*C error
+  } else if(temp > 25 && temp < 50){ //<0 to avoid -999.00*C error
     digitalWrite(HEATER, LOW);
     //Serial.println("Heater OFF");
   } else {
@@ -249,14 +253,14 @@ void updateThingSpeak(String tsData)
 {
   if (client.connect(thingSpeakAddress, 80)){
 
-    client.println("POST /update?api_key=POJ06PXJKR76NB39 HTTP/1.1");
-    client.println("Host: api.thingspeak.com");
-    client.println("User-Agent: Arduino/1.0");
-    client.println("Connection: close");
-    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.print("POST /update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("X-THINGSPEAKAPIKEY: "+myThingSpeakKey+"\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
     client.print("Content-Length: ");
     client.print(tsData.length());
-    client.println();
+    client.print("\n\n");
     client.print(tsData);
     
     lastConnectionTime = millis();
@@ -295,6 +299,8 @@ void setup() {
     
 }
 
+void(* resetFunc) (void) = 0; //declare reset function @ address 0
+
 void setDefaults(){
 
   // setting pins to output mode.
@@ -312,30 +318,24 @@ void setDefaults(){
   // water symbol
   lcd.createChar(1, water);
 
+  //celcius symbol
+  lcd.createChar(2, celsiusSymbol);
+
   delay(1000);
   float pass = checkWaterTemp();
   if( pass > 1)
     temperature = pass;
 
-
 }
 
 void controlMoonLeds(unsigned long myTime){
 
-  byte first = brightness(23*HOUR, (23*HOUR + 60*MINUTE), "moon");
-  byte second = brightness(0, 30*MINUTE, "moon");
-  
-  if(first == 255 || second == 255){
-    digitalWrite(MOONLIGHTS, HIGH);
-  } else if(first == 0 && second == 0){
+  byte first = brightness(myTime, 22*HOUR+45*MINUTE, 23*HOUR+45*MINUTE, "moon");
+
+  if(first == 0){
     digitalWrite(MOONLIGHTS, LOW);
   } else{
-      if(first > second){
-        analogWrite(MOONLIGHTS_PWM, first);
-      }
-      else if(second > first){
-        analogWrite(MOONLIGHTS_PWM, second);
-      }
+      digitalWrite(MOONLIGHTS, HIGH);
   }
 }
 
@@ -343,41 +343,43 @@ int controlLeds(unsigned long myTime){
 
   int openFan = 0;
 
-  byte first = brightness(10*HOUR, 14*HOUR+30*MINUTE, "led");
-  //byte second = brightness(18*HOUR, 23*HOUR , "led");
-  byte second = brightness(18*HOUR, 22*HOUR+30*MINUTE , "led");
-  if(first == 255 || second == 255){
+  byte first = brightness(myTime, 10*HOUR+30*MINUTE, 13*HOUR+45*MINUTE, "led");
+  byte second = brightness(myTime, 18*HOUR, 22*HOUR+45*MINUTE, "led");
+  //revert the pwm value because of the pwm curcuit
+  second = map(second, PWM_MIN, PWM_MAX, PWM_MAX, DESIRED_BRIGHTNESS);
+  first = map(first, PWM_MIN, PWM_MAX, PWM_MAX, DESIRED_BRIGHTNESS);
+
+  if(first == PWM_MIN || second == PWM_MIN){
     digitalWrite(LIGHTS, HIGH);
+    analogWrite(LIGHTS_PWM, DESIRED_BRIGHTNESS);
     openFan = 1;
 
-  } else if(first == 0 && second == 0){
-    digitalWrite(LIGHTS, LOW);
-    openFan = 0;
+  } else if(first == PWM_MAX && second == PWM_MAX){
+      digitalWrite(LIGHTS, LOW);
+      openFan = 0;
 
   } else{
-      if(first > second){
+      digitalWrite(LIGHTS, HIGH);
+      if(first < second){
         analogWrite(LIGHTS_PWM, first);
       }
-      else if(second > first){
+      else if(second < first){
         analogWrite(LIGHTS_PWM, second);
       }
-      
-    openFan = 1;
+      openFan = 1;
   }
   
   return openFan;
 }
 
-int controlCO2(){
+int controlCO2(unsigned long myTime){
 
   int openCo2 = 0;
 
-    // co2 control 
-  if ( ((hour() >= 9 && minute() >= 0) && (hour() <= 14 &&  minute() <= 59)) ||
-       ((hour() >= 17 && minute() >= 0) && (hour() <= 22 &&  minute() <= 59))){
-      //open co2
-      digitalWrite(CO2, HIGH);
-      openCo2 = 1;
+  if ((myTime >= 9*HOUR+30*MINUTE  && myTime <= 13*HOUR+30*MINUTE) || (myTime >= 17*HOUR && myTime <= 22*HOUR+30*MINUTE) ){
+    //open co2
+    digitalWrite(CO2, HIGH);
+    openCo2 = 1;
   } else{
       //close co2
       digitalWrite(CO2, LOW);
@@ -386,57 +388,36 @@ int controlCO2(){
   return openCo2;
 }
 
-byte brightness(unsigned long fadeUpStart, unsigned long fadeDownStart, String myLed)
+byte brightness(unsigned long lastDaySeconds, unsigned long fadeUpStart, unsigned long fadeDownStart, String myLed)
 {
-
-  unsigned long lastDaySeconds = hour() * HOUR + minute() * MINUTE + second();
   
-  if (lastDaySeconds >= fadeUpStart  && lastDaySeconds <= fadeDownStart){
+  if (lastDaySeconds >= fadeUpStart + MINUTE*15 && lastDaySeconds <= fadeDownStart){
     return TARGET_BRIGHTNESS;
   }
 
   // Dawn:  fade up the light
-  
-  if (lastDaySeconds >= fadeUpStart && lastDaySeconds <= fadeUpStart + 30*MINUTE)  // Fading up
+  if (lastDaySeconds >= fadeUpStart && lastDaySeconds <= fadeUpStart + 15*MINUTE)  // Fading up
   {
-   /* Serial.print("myLed:");
-    Serial.println(myLed);
-    Serial.print("fadeUpStart:");
-    Serial.println(fadeUpStart);
-    Serial.print("fadeDownStart");
-    Serial.println(fadeDownStart);
-    Serial.print("time:");
-    Serial.println(time);
-    Serial.println("if2----------------");*/
     unsigned long seconds = lastDaySeconds - fadeUpStart;  // Number of seconds into the fade time 
-    return TARGET_BRIGHTNESS * seconds / (MINUTE*30);  // Fade up based on portion of interval completed.
+    return TARGET_BRIGHTNESS * seconds / (MINUTE*15);  // Fade up based on portion of interval completed.
   }
 
   // Evening: Fade down the light
-  if (lastDaySeconds >= fadeDownStart && lastDaySeconds <= fadeDownStart + 30*MINUTE)  // Fading down
+  if (lastDaySeconds >= fadeDownStart && lastDaySeconds <= fadeDownStart + 15*MINUTE)  // Fading down
   {
-    /*
-    Serial.print("myLed:");
-    Serial.println(myLed);
-    Serial.print("fadeUpStart:");
-    Serial.println(fadeUpStart);
-    Serial.print("fadeDownStart");
-    Serial.println(fadeDownStart);
-    Serial.print("time:");
-    Serial.println(time);
-    Serial.println("if3----------------");*/
-    unsigned long seconds = (fadeDownStart + (MINUTE*30)) - lastDaySeconds;  // Number of seconds remaining in the fade time 
-    return TARGET_BRIGHTNESS * seconds / (MINUTE*30);  // Fade down based on portion of interval left.
+    unsigned long seconds = (fadeDownStart + (MINUTE*15)) - lastDaySeconds;  // Number of seconds remaining in the fade time 
+    return TARGET_BRIGHTNESS * seconds / (MINUTE*15);  // Fade down based on portion of interval left.
   }
 
-
   // The remaining times are night and the lights is off
-
   return 0;  // Shouldn't get here
 }
 
 void loop() {
 
+  unsigned long today = hour() * HOUR + minute() * MINUTE + second();
+
+  //check the water tempature every 5 secs.
   if (now() % 5 == 0){
     float temp = checkWaterTemp();
     if(temp > 0 && temp < 40)
@@ -444,17 +425,11 @@ void loop() {
   }
 
   heaterControl(temperature);
-  int co2Check = controlCO2(); 
-  if(now() % 2 == 0){
-    displaySystem(temperature, co2Check);  
-  }  
+  int co2Check = controlCO2(today); 
 
-  int fanCheck = controlLeds(now());
+  int fanCheck = controlLeds(today);
   fanControl(fanCheck);
-  controlMoonLeds(now());
-  //Serial.print("saat:");
-  //Serial.println(now());
-  //delay(1000);
+  controlMoonLeds(today);
 
   // Update the time via NTP server as often as the time you set at the top
   if(now()-ntpLastUpdate > ntpSyncTime) {
@@ -489,7 +464,7 @@ void loop() {
       client.stop();
     }
 
-  // Update ThingSpeak
+  // Update ThingSpeak channel with my aquarium features
   if(!client.connected() && (millis() - lastConnectionTime > updateThingSpeakInterval)){
 
       String waterTemp = String(temperature, DEC);
@@ -497,8 +472,7 @@ void loop() {
       String moonlights = String(digitalRead(MOONLIGHTS), DEC);
       String co2String = String(digitalRead(CO2), DEC);
       String heaterString = String(digitalRead(HEATER), DEC);
-      String fanString = String(digitalRead(FANS), DEC);
-
+      //String fanString = String(digitalRead(FANS), DEC);
 
       Serial.print("waterTemp:");
       Serial.println(temperature);
@@ -510,13 +484,9 @@ void loop() {
       Serial.println(co2String);
       Serial.print("heaterString:");
       Serial.println(heaterString);
-      Serial.print("fanString:");
-      Serial.println(fanString);
 
-     // updateThingSpeak("temperature=5");  
-      //updateThingSpeak("temperature="+waterTemp+"&co2="+co2String+"&fan="+fanString+"&dayLights="+daylights+"&moonLights="+moonlights+"&heater="+heaterString);
-      //updateThingSpeak("field1=11");
-      //updateThingSpeak("field1="+waterTemp+"&field2="+co2String+"&field3="+fanString+"&field4="+daylights+"&field5="+moonlights+"&field6="+heaterString);
+      //HTTP POST the values
+      //updateThingSpeak("field1="+waterTemp+"&field2="+co2String"+&field3="+daylights+"&field4="+moonlights+"&field5="+heaterString);
   }
   // Check if Arduino Ethernet needs to be restarted
   if (failedCounter > 3 ) {startEthernet();}
@@ -643,6 +613,7 @@ unsigned long sendNTPpacket(IPAddress& address){
 }
 
 void getTimeNTP(){
+
   // Ethernet shield and NTP setup
    int i = 0;
    int DHCP = 0;
@@ -655,7 +626,8 @@ void getTimeNTP(){
    }
    if(!DHCP){
     Serial.println("DHCP FAILED");
-     for(;;); //Infinite loop because DHCP Failed
+    resetFunc();  //call reset
+    // for(;;); //Infinite loop because DHCP Failed
    }
    Serial.println("DHCP Success");
    
